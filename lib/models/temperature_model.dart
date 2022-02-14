bea839cc54cd0c2b6ef024b08dc3b60a81f0c395
sunskmn <79347608+sunskmn@users.noreply.github.com>
2022-02-14 13:20:12 +0700

    last update success

--- lib/models/temperature_model.dart
+++ lib/models/temperature_model.dart
@@ -1,21 +1,21 @@
-class TemperatureModel {
-  final String Day;
-  final int index;
-  final double temperature;
-  final String time;
+  class TemperatureModel {
+    final String Day;
+    final int index;
+    final double temperature;
+    final String time;
 
-  TemperatureModel(
-      {this.Day = '', this.index = 0, this.temperature = 0, this.time = ''});
+    TemperatureModel(
+        {this.Day = '', this.index = 0, this.temperature = 0, this.time = ''});
 
-  factory TemperatureModel.formJson(Map<dynamic, dynamic> json) {
-    if (json == null) {
-      return new TemperatureModel();
+    factory TemperatureModel.formJson(Map<dynamic, dynamic> json) {
+      if (json == null) {
+        return new TemperatureModel();
+      }
+
+      return TemperatureModel(
+          Day: json['Day'],
+          index: json['index'],
+          temperature: json['temperature'].toDouble(),
+          time: json['time']);
     }
-
-    return TemperatureModel(
-        Day: json['Day'],
-        index: json['index'],
-        temperature: json['temperature'].toDouble(),
-        time: json['time']);
   }
-}
